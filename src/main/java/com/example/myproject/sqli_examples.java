725a8f45df5779f55374b85b28f74eec189f2816
gpsandhu23 <134000058+gpsandhu23@users.noreply.github.com>
2023-07-06 19:35:53 -0700

    Update sqli_examples.java
    Removed a few redundant examples to keep the demo clean

--- src/main/java/com/example/myproject/sqli_examples.java
+++ src/main/java/com/example/myproject/sqli_examples.java
@@ -19,29 +19,17 @@ String query = "INSERT INTO users (username, password) VALUES (?, ?)";
 PreparedStatement statement = connection.prepareStatement(query); statement.setString(1, username); 
 statement.setString(2, password); statement.addBatch(); statement.executeBatch();
 
-
 // Example 5
-String userCheckQuery = "{call sp_getUserDetails(?)}";
-CallableStatement cs = connection.prepareCall(userCheckQuery);
-cs.setString(1, username);
-ResultSet rs = cs.executeQuery();
-
-// Example 6
 String query = "SELECT * FROM users WHERE username LIKE '" + username + "%'";
 Statement statement = connection.createStatement();
 ResultSet resultSet = statement.executeQuery(query);
 
-// Example 7
+// Example 6
 String query = "UPDATE users SET password = '" + newPassword + "' WHERE username = '" + username + "'";
 Statement statement = connection.createStatement();
 int rowsAffected = statement.executeUpdate(query);
 
-// Example 8
+// Example 7
 String query = "DELETE FROM users WHERE username = '" + username + "'";
-Statement statement = connection.createStatement();
-int rowsAffected = statement.executeUpdate(query);
-
-// Example 9
-String query = "INSERT INTO users(username, password) VALUES('" + username + "', '" + password + "')";
 Statement statement = connection.createStatement();
 int rowsAffected = statement.executeUpdate(query);
