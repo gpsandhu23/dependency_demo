cdd6da8ffd1f0eafae99c09aac344e0b3435ac32
gpsandhu23 <134000058+gpsandhu23@users.noreply.github.com>
2023-07-06 19:31:42 -0700

    Update sqli_examples.java
    Added some examples that don't have sqli

--- src/main/java/com/example/myproject/sqli_examples.java
+++ src/main/java/com/example/myproject/sqli_examples.java
@@ -3,34 +3,45 @@ String query = "SELECT * FROM users WHERE username = '" + username + "' AND pass
 Statement statement = connection.createStatement();
 ResultSet resultSet = statement.executeQuery(query);
 
-// Example 2
+// Example 2 
+String query = "SELECT * FROM users WHERE username = ? AND password = ?"; 
+PreparedStatement statement = connection.prepareStatement(query); statement.setString(1, username); 
+statement.setString(2, password); ResultSet resultSet = statement.executeQuery();
+
+// Example 3
 String query = "INSERT INTO users (username, password) VALUES ('" + username + "', '" + password + "')";
 Statement statement = connection.createStatement();
 statement.addBatch(query);
 statement.executeBatch();
 
-// Example 3
+// Example 4 
+String query = "INSERT INTO users (username, password) VALUES (?, ?)"; 
+PreparedStatement statement = connection.prepareStatement(query); statement.setString(1, username); 
+statement.setString(2, password); statement.addBatch(); statement.executeBatch();
+
+
+// Example 5
 String userCheckQuery = "{call sp_getUserDetails(?)}";
 CallableStatement cs = connection.prepareCall(userCheckQuery);
 cs.setString(1, username);
 ResultSet rs = cs.executeQuery();
 
-// Example 4
+// Example 6
 String query = "SELECT * FROM users WHERE username LIKE '" + username + "%'";
 Statement statement = connection.createStatement();
 ResultSet resultSet = statement.executeQuery(query);
 
-// Example 5
+// Example 7
 String query = "UPDATE users SET password = '" + newPassword + "' WHERE username = '" + username + "'";
 Statement statement = connection.createStatement();
 int rowsAffected = statement.executeUpdate(query);
 
-// Example 6
+// Example 8
 String query = "DELETE FROM users WHERE username = '" + username + "'";
 Statement statement = connection.createStatement();
 int rowsAffected = statement.executeUpdate(query);
 
-// Example 7
+// Example 9
 String query = "INSERT INTO users(username, password) VALUES('" + username + "', '" + password + "')";
 Statement statement = connection.createStatement();
 int rowsAffected = statement.executeUpdate(query);
