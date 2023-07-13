d1a74c3c123bc52bbf970f0558989cd2a081f687
gpsandhu23 <134000058+gpsandhu23@users.noreply.github.com>
2023-07-13 14:34:33 -0700

    Update xxe.java
    added another example

--- src/main/java/com/example/myproject/xxe.java
+++ src/main/java/com/example/myproject/xxe.java
@@ -4,3 +4,8 @@ DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 DocumentBuilder db = dbf.newDocumentBuilder();
 
 db.parse(new InputSource(new StringReader(xml)));
+
+import javax.xml.stream.*;
+
+XMLInputFactory xif = XMLInputFactory.newFactory();
+XMLStreamReader xsr = xif.createXMLStreamReader(new StringReader(xml));
