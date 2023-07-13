b9d8a8f9473fbf55501e4b6924ead26769da8c96
gpsandhu23 <134000058+gpsandhu23@users.noreply.github.com>
2023-07-12 18:04:39 -0700

    Update sqli_examples.java

--- src/main/java/com/example/myproject/sqli_examples.java
+++ src/main/java/com/example/myproject/sqli_examples.java
@@ -2,3 +2,8 @@
 String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
 Statement statement = connection.createStatement();
 ResultSet resultSet = statement.executeQuery(query);
+
+//Example 2
+String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+Statement statement = connection.createStatement();
+ResultSet resultSet = statement.executeQuery(query);
