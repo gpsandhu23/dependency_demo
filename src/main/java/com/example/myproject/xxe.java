c1c13b7cb128829a9d8fd87aa12615981185a49e
root <root@eb1e71c29c40>
2023-07-17 23:44:19 +0000

    Fix security vulnerabilities

--- src/main/java/com/example/myproject/xxe.java
+++ src/main/java/com/example/myproject/xxe.java
@@ -1,11 +1,19 @@
 import javax.xml.parsers.*;
 
 DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
 DocumentBuilder db = dbf.newDocumentBuilder();
 
-db.parse(new InputSource(new StringReader(xml)));
+InputSource is = new InputSource(new StringReader(xml));
+dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+db.parse(is);
 
 import javax.xml.stream.*;
 
 XMLInputFactory xif = XMLInputFactory.newFactory();
+xif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+xif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
 XMLStreamReader xsr = xif.createXMLStreamReader(new StringReader(xml));
