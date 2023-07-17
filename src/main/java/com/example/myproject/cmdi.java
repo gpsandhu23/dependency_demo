d6f5246c4e45ee8fe43350d49e2663393a40baf4
gpsandhu23 <134000058+gpsandhu23@users.noreply.github.com>
2023-07-16 19:22:46 -0700

    Update cmdi.java

--- src/main/java/com/example/myproject/cmdi.java
+++ src/main/java/com/example/myproject/cmdi.java
@@ -6,22 +6,4 @@ public class CommandInjection {
         Runtime runtime = Runtime.getRuntime();
         runtime.exec("ping " + userInput);
     }
-
-    public static void processBuilderExec(String userInput) throws IOException {
-        ProcessBuilder pb = new ProcessBuilder("ping", userInput);
-        pb.start();
-    }
-
-    public static void shellCommandExec(String userInput) throws IOException {
-        Runtime runtime = Runtime.getRuntime();
-        runtime.exec("/bin/sh -c echo " + userInput);
-    }
-
-    public static void main(String[] args) throws IOException {
-        String userInput = args[0]; // user input is directly used
-
-        directRuntimeExec(userInput);
-        processBuilderExec(userInput);
-        shellCommandExec(userInput);
-    }
 }
