9c001768331413e471a0b5598c476b5cd0fafead
root <root@5bdeb009cdea>
2023-07-18 00:40:02 +0000

    Fix security vulnerabilities

--- src/main/java/com/example/myproject/cmdi.java
+++ src/main/java/com/example/myproject/cmdi.java
@@ -4,6 +4,7 @@ public class CommandInjection {
 
     public static void directRuntimeExec(String userInput) throws IOException {
         Runtime runtime = Runtime.getRuntime();
-        runtime.exec("ping " + userInput);
+        String[] cmd = {"/bin/sh", "-c", "ping " + userInput};
+runtime.exec(cmd);
     }
 }
