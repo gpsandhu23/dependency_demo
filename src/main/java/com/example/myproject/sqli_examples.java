7d7262f0c6e747a3a6ff80ccef4d5a5c386bbccd
gpsandhu23 <134000058+gpsandhu23@users.noreply.github.com>
2023-07-22 14:59:27 -0700

    Update sqli_examples.java
    Added 2 more examples

--- src/main/java/com/example/myproject/sqli_examples.java
+++ src/main/java/com/example/myproject/sqli_examples.java
@@ -1,3 +1,11 @@
 String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
 Statement statement = connection.createStatement();
 ResultSet resultSet = statement.executeQuery(query);
+
+String query = "DELETE FROM users WHERE id = '" + userId + "'";
+Statement statement = connection.createStatement();
+ResultSet resultSet = statement.executeQuery(query);
+
+String query = "UPDATE users SET email = '" + newEmail + "' WHERE username = '" + username + "'";
+Statement statement = connection.createStatement();
+ResultSet resultSet = statement.executeQuery(query);
