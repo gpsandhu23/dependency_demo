a8b77882140e545371cf9dd0e39c78c6a5f5d1f8
gpsandhu23 <gpsandhu23@gmail.com>
2023-08-04 17:58:06 -0700

    Fix security issues in local-repo-src-main-java-com-example-myproject-sqli_examples.java

--- src/main/java/com/example/myproject/sqli_examples.java
+++ src/main/java/com/example/myproject/sqli_examples.java
@@ -1,11 +1,13 @@
-String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
-Statement statement = connection.createStatement();
-ResultSet resultSet = statement.executeQuery(query);
+PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+pstmt.setString(1, username);
+pstmt.setString(2, password);
+ResultSet resultSet = pstmt.executeQuery();
 
-String query = "DELETE FROM users WHERE id = '" + userId + "'";
-Statement statement = connection.createStatement();
-ResultSet resultSet = statement.executeQuery(query);
+PreparedStatement pstmt = connection.prepareStatement("DELETE FROM users WHERE id = ?");
+pstmt.setString(1, userId);
+ResultSet resultSet = pstmt.executeQuery();
 
-String query = "UPDATE users SET email = '" + newEmail + "' WHERE username = '" + username + "'";
-Statement statement = connection.createStatement();
-ResultSet resultSet = statement.executeQuery(query);
+PreparedStatement pstmt = connection.prepareStatement("UPDATE users SET email = ? WHERE username = ?");
+pstmt.setString(1, newEmail);
+pstmt.setString(2, username);
+ResultSet resultSet = pstmt.executeQuery();
